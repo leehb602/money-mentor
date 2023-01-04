313f65db023aa36ad1b09fb530546bb7b6017b11
leehb602 <leehb602s@naver.com>
2023-01-04 15:04:32 +0900

    회원 MVC 추가

---
+++ spring_boot_FinalProject/src/main/java/com/spring_boot_momentor/service/IJoinUserService.java
@@ -0,0 +1,5 @@
+package com.spring_boot_momentor.service;
+
+public interface IJoinUserService {
+
+}
---
+++ spring_boot_FinalProject/src/main/java/com/spring_boot_momentor/service/JoinUserService.java
@@ -0,0 +1,8 @@
+package com.spring_boot_momentor.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinUserService {
+ 
+}
