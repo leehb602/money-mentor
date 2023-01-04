ed2448ec915ee78e2c3767e5cbec2e1a73dfe5db
leehb602 <leehb602s@naver.com>
2023-01-04 14:56:15 +0900

    이걸로 가져가

---
+++ spring_boot_FinalProject/src/main/java/com/spring_boot_momentor/WebConfig.java
@@ -0,0 +1,14 @@
+package com.spring_boot_momentor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**")
+		.addResourceLocations(""); // 각자 외부경로 적용 필!
+	}
+}
